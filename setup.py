fa9b3bd183fa5af6fa47a50c7745156d571064f2
Marcus Medley <mdmeds@gmail.com>
2016-07-27 12:21:31 -0500

    Add Travis Autodeploy to PyPI

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='requests_logger',
-    version='0.0.1',
+    version='0.0.2',
     description='Wrapper around Requests to add logging',
     author='Marcus Medley',
     author_email='mdmeds@gmail.com',
